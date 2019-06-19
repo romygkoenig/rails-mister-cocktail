ab803914b0b6bca5660d12829d1460ee55f3b770
romy koenig <romyg.koenig@gmail.com>
2019-06-19 11:04:57 +0300

    set up complete

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+const { environment } = require('@rails/webpacker')
+
+# // Bootstrap 4 has a dependency over jQuery & Popper.js:
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+module.exports = environment
