70743e9cab06623b34432565590efadeda9b0ed5
romy koenig <romyg.koenig@gmail.com>
2019-07-14 15:35:51 +0300

    add photo requirement

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true, presence: true
+  validates :photo, presence: true
 
   mount_uploader :photo, PhotoUploader
 end
